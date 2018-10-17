66fc157f55370745be7bc312d96b6a3358d30911
Yeonha Park <yyuunn0044@gmail.com>
2018-10-17 13:44:44 +0900

    핀볼_게임 success

--- SwExpertAcademy/핀볼_게임/solution.py
+++ SwExpertAcademy/핀볼_게임/solution.py
@@ -9,10 +9,10 @@ digit_direction = {
     "R": (0, 1)
 }
 side = {
-    1: { "D":"R", "L":"U" },
-    2: {"U":"R", "L":"D"},
-    3:{"U":"L", "R":"D"},
-    4:{"D":"L", "R":"U"},
+    1: {"D": "R", "L": "U"},
+    2: {"U": "R", "L": "D"},
+    3: {"U": "L", "R": "D"},
+    4: {"D": "L", "R": "U"},
 }
 flat = {
     "D": "U",
@@ -21,66 +21,72 @@ flat = {
     "L": "R"
 }
 
-def getIndex(i):
+def get_index(kind):
     indexes = []
     for row in range(N):
         for col in range(N):
-            if board[row][col] == i:
+            if board[row][col] == kind:
                 indexes.append((row, col))
     return indexes
-def outOfBoard(r, c):
+
+def out_of_board(dst):
+    r, c = dst[0], dst[1]
     if r < 0 or r > N-1:
         return True
     elif c < 0 or c > N-1:
         return True
     else:
         return False
-def go(start, direction):
-    print(start, direction)
-    point = 0
-    r, c = start
-    r_diff, c_diff = digit_direction[direction]
-    n_r, n_c = r+r_diff, c+c_diff
-    if outOfBoard(n_r, n_c):
-        n_r, n_c = r, c
-        end = board[n_r][n_c]
-        next_direction = flat[direction]
-        point += 1
-    else:
-        end = board[n_r][n_c]
-        next_direction = direction
-    if end >= 1 and end <= 5:
-        try:
-            next_direction = side[end][direction]
-        except KeyError:
-            next_direction = flat[direction]
-        point += 1
-    elif end >= 6 and end <= 10:
-        end = getIndex(end).remove((n_r, n_c))[0]
-    elif end == -1:
-        point = -1
-    else:
-        pass
 
-    return ((n_r, n_c), next_direction, point)
+def go(src, src_direc):
+    point = 0
+    r, c = src
+    r_diff, c_diff = digit_direction[src_direc]
+    dst = (r+r_diff, c+c_diff)
+    dst_direc = src_direc
+
+    if out_of_board(dst):
+        dst, dst_direc, tmp_point = go(dst, flat[dst_direc])
+        point += 1 + tmp_point
+    else:
+        kind = board[dst[0]][dst[1]]
+        if 1 <= kind <= 5:
+            try:
+                dst_direc = side[kind][src_direc]
+            except KeyError:
+                dst_direc = flat[src_direc]
+            point += 1
+        elif 6 <= kind <= 10:
+            wormholes = get_index(kind)
+            wormholes.remove(dst)
+            dst = wormholes[0]
+        elif kind == -1:
+            dst = None
+        else:
+            pass
+
+    return dst, dst_direc, point
+
+
 def totalRoute(unvisited):
     route = []
     total_point = 0
     start = src = unvisited[0:2]
     src_direc = unvisited[2]
-    while not start in route:
+    while True:
+        route += [(src[0], src[1], src_direc)]
+        #print(src, src_direc)
         dst, dst_direc, point = go(src, src_direc)
-        print(dst, dst_direc, point)
-        if point == -1:
-            break
+        #print(dst, dst_direc, point)
         total_point += point 
         src = dst
         src_direc = dst_direc
-        route += [(src[0], src[1], src_direc)]
-    print("this route",total_point)
-    return total_point
+        if src is None or start == src:
+            break
+    #print("this route",total_point)
+    return route, total_point
 
-def getUnvisitedRoute():
+def getAllDeparture():
     unvisited = []
     for r in range(N):
         for c in range(N):
@@ -88,22 +94,16 @@ def getUnvisitedRoute():
                 unvisited.extend([(r, c, direc) for direc in DIRECTION])
     return unvisited
 
-def removeUnvisited(unvisited, route):
-    for r in route:
-        other = tuple(r[0:2] + flat[r[2]])
-        unvisited.remove(r)
-        unvisited.remove(other)
-    return unvisited
-
-def solution(N, board):
+def solution():
     maxScore = 0
-    unvisited = getUnvisitedRoute()
+    unvisited = getAllDeparture()
     while len(unvisited) != 0:
         u = unvisited[0]
         route, score = totalRoute(u)
-        unvisited = removeUnvisited(unvisited, route)
+        unvisited = unvisited[1:]
         if score > maxScore:
             maxScore = score
+
     return maxScore
 
 if __name__ == "__main__":
@@ -118,7 +118,7 @@ if __name__ == "__main__":
         board = []
         for i in range(N):
             board.append([int(elem) for elem in input().split()])
-        actual = "#"+str(test_case)+" "+str(solution(N, board))
+        actual = "#"+str(test_case)+" "+str(solution())
         print(actual)
         
 
