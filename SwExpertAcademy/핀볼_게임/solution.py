5f285cf5e6923308e981c53a8433a1447f9663e9
Yeonha Park <yyuunn0044@gmail.com>
2018-10-17 13:53:52 +0900

    핀볼_게임 prettify

--- SwExpertAcademy/핀볼_게임/solution.py
+++ SwExpertAcademy/핀볼_게임/solution.py
@@ -1,27 +1,27 @@
 import sys
 import os
-from pprint import pprint
+
 DIRECTION = ['U', 'D', 'L', 'R']
-digit_direction = {
+DIRECTION_TO_DIFF= {
     "U": (-1, 0),
     "D": (1, 0),
     "L": (0, -1),
     "R": (0, 1)
 }
-side = {
+SKEW = {
     1: {"D": "R", "L": "U"},
     2: {"U": "R", "L": "D"},
     3: {"U": "L", "R": "D"},
     4: {"D": "L", "R": "U"},
 }
-flat = {
+OPPOSITE = {
     "D": "U",
     "U": "D",
     "R": "L",
     "L": "R"
 }
 
-def get_index(kind):
+def getIndex(kind):
     indexes = []
     for row in range(N):
         for col in range(N):
@@ -29,7 +29,7 @@ def get_index(kind):
                 indexes.append((row, col))
     return indexes
 
-def out_of_board(dst):
+def outOfBoard(dst):
     r, c = dst[0], dst[1]
     if r < 0 or r > N-1:
         return True
@@ -38,26 +38,34 @@ def out_of_board(dst):
     else:
         return False
 
+def getAllDeparture():
+    unvisited = []
+    for r in range(N):
+        for c in range(N):
+            if board[r][c] == 0:
+                unvisited.extend([((r, c), direc) for direc in DIRECTION])
+    return unvisited
+
 def go(src, src_direc):
     point = 0
     r, c = src
-    r_diff, c_diff = digit_direction[src_direc]
+    r_diff, c_diff = DIRECTION_TO_DIFF[src_direc]
     dst = (r+r_diff, c+c_diff)
     dst_direc = src_direc
 
-    if out_of_board(dst):
-        dst, dst_direc, tmp_point = go(dst, flat[dst_direc])
+    if outOfBoard(dst):
+        dst, dst_direc, tmp_point = go(dst, OPPOSITE[dst_direc])
         point += 1 + tmp_point
     else:
         kind = board[dst[0]][dst[1]]
         if 1 <= kind <= 5:
             try:
-                dst_direc = side[kind][src_direc]
+                dst_direc = SKEW[kind][src_direc]
             except KeyError:
-                dst_direc = flat[src_direc]
+                dst_direc = OPPOSITE[src_direc]
             point += 1
         elif 6 <= kind <= 10:
-            wormholes = get_index(kind)
+            wormholes = getIndex(kind)
             wormholes.remove(dst)
             dst = wormholes[0]
         elif kind == -1:
@@ -67,43 +75,26 @@ def go(src, src_direc):
 
     return dst, dst_direc, point
 
-
 def totalRoute(unvisited):
-    route = []
     total_point = 0
-    start = src = unvisited[0:2]
-    src_direc = unvisited[2]
+    start = src = unvisited[0]
+    src_direc = unvisited[1]
     while True:
-        route += [(src[0], src[1], src_direc)]
-        #print(src, src_direc)
         dst, dst_direc, point = go(src, src_direc)
-        #print(dst, dst_direc, point)
-        total_point += point 
+        total_point += point
         src = dst
         src_direc = dst_direc
         if src is None or start == src:
             break
-    #print("this route",total_point)
-    return route, total_point
-
-def getAllDeparture():
-    unvisited = []
-    for r in range(N):
-        for c in range(N):
-            if board[r][c] == 0:
-                unvisited.extend([(r, c, direc) for direc in DIRECTION])
-    return unvisited
+    return total_point
 
 def solution():
     maxScore = 0
     unvisited = getAllDeparture()
-    while len(unvisited) != 0:
-        u = unvisited[0]
-        route, score = totalRoute(u)
-        unvisited = unvisited[1:]
+    for u in unvisited:
+        score = totalRoute(u)
         if score > maxScore:
             maxScore = score
-
     return maxScore
 
 if __name__ == "__main__":
@@ -121,6 +112,5 @@ if __name__ == "__main__":
         actual = "#"+str(test_case)+" "+str(solution())
         print(actual)
         
-
 board = []
-N = 0 
+N = 0
