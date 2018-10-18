d2a1feaa5dbb489814b30a4c5f995cccc52b7e6e
Yeonha Park <yyuunn0044@gmail.com>
2018-10-18 22:03:31 +0900

    원자_소멸_시뮬레이션 ongoing

--- SwExpertAcademy/원자_소멸_시뮬레이션_ongoing/solution.py
+++ SwExpertAcademy/원자_소멸_시뮬레이션_ongoing/solution.py
@@ -9,34 +9,56 @@ INT_TO_DIFF= {
 }
 def outOfBound(atom_xy):
     x, y = atom_xy
-    if (-1000 <= x <= 1000) and (-1000 <= y <= 1000):
+    if (-2000 <= x <= 2000) and (-2000 <= y <= 2000):
         return False
     return True
 
-'''
+def removeGoneAtoms():
+
+    upMax = 1000
+    downMax = -1000
+    leftMax = -1000
+    rightMax = 1000
+    for atom in atoms:
+        x, y = atom[0]
+        upMax = upMax if y <= upMax else y
+        downMax = downMax if y >= downMax else y
+        leftMax = leftMax if x >= leftMax else x
+        rightMax = rightMax if x <= rightMax else x
+    for atom in atoms:
+        x, y = atom[0]
+        direction = atom[1]
+        if y == upMax and direction == (0, 1) \
+            or y == downMax and direction == (0, -1) \
+            or x == leftMax and direction == (-1, 0) \
+            or x == rightMax and direction == (1, 0):
+            del atom
+
+
 def atomTimeRuns(atom):
     src, diff, energy = atom
     dst = (src[0] + diff[0], src[1] + diff[1])
     return dst, diff, energy
-'''
+
 def timeRuns():
     global atoms
     energy = 0
     for i in range(100):
-        atoms = [[(atom[0][0] + atom[1][0], atom[0][1] + atom[1][1]), atom[1], atom[2]]
-                    for atom in atoms if not outOfBound(atom[0])]
+        atoms = [atomTimeRuns(atom) for atom in atoms if not outOfBound(atom[0])]
         locas = [atom[0] for atom in atoms]
         duplicates = [loca for loca in locas if locas.count(loca) > 1]
         removed_atoms = [atom for atom in atoms if atom[0] in duplicates]
         for removed in removed_atoms:
             energy += removed[2]
             atoms.remove(removed)
+
     return energy
 
 def solution():
     energy = 0
     while len(atoms) != 0:
         energy += timeRuns()
+        removeGoneAtoms()
     return energy
 
 if __name__ == "__main__":
@@ -51,7 +73,7 @@ if __name__ == "__main__":
         atoms = []
         for i in range(N):
             x, y, dir, energy = input().split()
-            atoms.append([(int(x), int(y)), INT_TO_DIFF[dir], int(energy)])
+            atoms.append([(int(x)*2, int(y)*2), INT_TO_DIFF[dir], int(energy)])
         actual = "#"+str(test_case)+" "+str(solution())
         print(actual)
         
