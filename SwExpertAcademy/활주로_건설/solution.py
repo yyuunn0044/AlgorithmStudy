3991f1814eb16d8c4b3cebba41f8adc9569db6f6
Yeonha Park <yyuunn0044@gmail.com>
2018-10-19 13:50:51 +0900

    활주로_건설 complete

--- SwExpertAcademy/활주로_건설/solution.py
+++ SwExpertAcademy/활주로_건설/solution.py
@@ -3,55 +3,43 @@ import os
 
 def canBuild(line, RUNWAY_LEN):
     N = len(line)
-    diff = []
     prev = line[0]
-    for this in line[1:]:
-        diff.append(this-prev)
-        prev = this
-    #print(diff)
     built = []
-    for i in range(len(diff)):
-        if diff[i] == 0:
+    for i in range(1, N):
+        this = line[i]
+        diff = this-prev
+        if diff == 0:
             pass
-        elif diff[i] == 1:
-            
-            candidate = [j for j in range(max(0, i-RUNWAY_LEN+1), i)]
-            #print(candidate)
-            if len(candidate) != RUNWAY_LEN -1:
+        elif diff == 1:
+            candidate = range(max(0, i-RUNWAY_LEN), i)
+            if len(candidate) != RUNWAY_LEN:
                 return 0
-            if not all(diff[c] == 0 and (c not in built) for c in candidate):
+            if not all(line[c] == this-1 and (c not in built) for c in candidate):
                 return 0
             built += candidate
             
-        elif diff[i] == -1:
-            
-            candidate = [j for j in range(i+1, min(i+RUNWAY_LEN, N-1))]
-            if len(candidate) != RUNWAY_LEN -1:
+        elif diff == -1:
+            candidate = range(i, min(i+RUNWAY_LEN, N))
+            if len(candidate) != RUNWAY_LEN :
                 return 0
-            if not all(diff[c] == 0 and (c not in built) for c in candidate):
+            if not all(line[c] == this and (c not in built) for c in candidate):
                 return 0
             built += candidate
         else:
             return 0
-        #print("build?", built)
+        prev = this
     return 1
 
 def solution(terrain, RUNWAY_LEN):
-    #print(RUNWAY_LEN, "------------------")
     N = len(terrain)
     num = 0
     for t in terrain:
-        #print(t)
         num += canBuild(t, RUNWAY_LEN)
-        #print("\n")
-        #print(num)
     vertical_terrain = []
     for i in range(N):
         vertical_terrain.append([t[i] for t in terrain])
     for v_t in vertical_terrain:
         num += canBuild(v_t, RUNWAY_LEN)
-            
-
     return num
 
 if __name__ == "__main__":
